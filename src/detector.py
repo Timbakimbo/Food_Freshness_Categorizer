4dd64c6017e5b6e90e4c56e0768dc99f70a188d6
tbsfrz <tobias.franz@fau.de>
2026-06-17 20:05:58 +0200

    add NMS and IOU for object detection

--- src/detector.py
+++ src/detector.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 
 LOG_DIR = os.path.join(os.path.dirname(__file__), "..", "logs")
 IMG_SIZE = (224, 224)
-STRIDE = 64
-SCORE_THRESHOLD = 0.25
+STRIDE = 128 # zu klein: 16, 32, 64
+SCORE_THRESHOLD = 0.22 # zu hoch: 0.5, 0.3, 0.25
 
 def load_model() -> keras.Model:
     """Feature-Extraktor."""
@@ -30,6 +30,8 @@ def load_model() -> keras.Model:
     return backbone
     # return tf.keras.models.load_model("models/freshify_baseline_with_new_raw.keras") # = 0 Objekte
 
+def get_score(obj):
+    return obj["score"]
 
 class Detector:
     def __init__(self, window_size: tuple[int, int] = IMG_SIZE):
@@ -37,7 +39,59 @@ class Detector:
         self.window_size = window_size
         self.stride = STRIDE
         self.score_threshold = SCORE_THRESHOLD
+        self.nms_threshold = 0.2 # zu hoch: 0.5, 0.3
         self.model = load_model()
+     
+    @staticmethod   
+    def _iou(box_a: list[int], box_b: list[int]) -> float:
+        """Berechnet die Überlappung zweier Boxen."""
+        # https://learnopencv.com/intersection-over-union-iou-in-object-detection-and-segmentation/
+        ax1, ay1, ax2, ay2 = box_a
+        bx1, by1, bx2, by2 = box_b
+
+        i_x1 = max(ax1, bx1)
+        i_y1 = max(ay1, by1)
+        i_x2 = min(ax2, bx2)
+        i_y2 = min(ay2, by2)
+
+        i_w = max(0, i_x2 - i_x1)
+        i_h = max(0, i_y2 - i_y1)
+
+        intersection = i_w * i_h
+
+        area_a = (ax2 - ax1) * (ay2 - ay1)
+        area_b = (bx2 - bx1) * (by2 - by1)
+
+        union = area_a + area_b - intersection
+        if union > 0:
+            return intersection / union
+        else: 
+            return 0.0
+
+    def _non_max_suppression(self, objects: list[dict], iou_threshold: float = 0.5,) -> list[dict]:
+        """NMS-Algorithmus."""
+        if not objects:
+            return []
+
+        objects = sorted(
+            objects,
+            key=get_score,
+            reverse=True,
+        )
+
+        keep = []
+
+        while objects:
+            best = objects.pop(0)
+            keep.append(best)
+
+            objects = [
+                obj
+                for obj in objects
+                if self._iou(best["box"], obj["box"]) < iou_threshold
+            ]
+
+        return keep
 
     def _sliding_window(self, img_w: int, img_h: int):
         win_w, win_h = self.window_size
@@ -74,17 +128,24 @@ class Detector:
             if score < self.score_threshold:
                 continue
             
-            # DEBUG:
-            # stem = f"detection_{(score * 100)}"
-            # crop.save(os.path.join(LOG_DIR, f"{stem}.png"))
-            
-            # CROPS mit Feature=Gemüse/Obst
             results.append({
                 "box": [x, y, x + win_w, y + win_h],
-                "label": "Produkt", #TODO: Wofür?
+                "label": "Produkt", #TODO: remove -> predict: ediable/non_ediable
                 "score": score,
             })
+        
+        results = self._non_max_suppression(results, iou_threshold=self.nms_threshold)
+        results.sort(key=get_score, reverse=True)
 
+        #DEBUG:
+        for id, obj in enumerate(results):
+            x1, y1, x2, y2 = obj["box"]
+            crop = image.crop((x1, y1, x2, y2))
+            score = obj["score"]
+            
+            name = f"detection_{id:02d}_{score:.3f}"
+            crop.save(os.path.join(LOG_DIR, f"{name}.png"))
+        
         return results
 
 
@@ -94,8 +155,9 @@ def detect_objects(image: Image.Image) -> list[dict]:
     return detector.detect_objects(image)
 
 # DEBUG:
-# if __name__ == "__main__":
-#     img = Image.open("data/gemuese-box.png")
-#     d = Detector()
-#     detections = d.detect_objects(img)
-#     print(f"detect_objects: {len(detections)} detections found.")
+if __name__ == "__main__":
+    img = Image.open("data/gemuese-box.png")
+    # img = Image.open("data/train/edible/banane/banana-e-k (3).png")
+    d = Detector()
+    objects = d.detect_objects(img)
+    print(f"detect_objects: {len(objects)} objects found.")
